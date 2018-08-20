7d74e939fa40705731085d905269f7dca3c609bc
RiparianCowboy <33729775+RiparianCowboy@users.noreply.github.com>
2018-08-20 14:59:39 -0700

    dumping whatever changes were made on macbook

--- school-sim.py
+++ school-sim.py
@@ -39,10 +39,10 @@ game_screen = []
 ui_screen = []
 
 #game object icons
-filler = " "
-player_icon = "@"
-door_icon = "▒"
-wall_icon = "█"
+game_icons = {"filler" : " ",
+			  "player" : "@", 
+			  "door" : "▒",
+			  "wall" : "█"}
 
 #make the array game_map and fill it with filler arrays
 def make_map():
@@ -50,7 +50,7 @@ def make_map():
 		game_map.append([])
 	for a in range(map_y):
 		for b in range(map_x):
-			game_map[a].append([["filler", filler]])
+			game_map[a].append([["filler", game_icons["filler"]]])
 
 #make the array game_screen and fill it with filler
 def make_screen():
@@ -58,7 +58,7 @@ def make_screen():
 		game_screen.append([])
 	for a in range(screen_y):
 		for b in range(screen_x):
-			game_screen[a].append(filler)
+			game_screen[a].append(game_icons["filler"])
 
 #copy icons from game_map into game_screen
 def update_screen():
@@ -130,29 +130,33 @@ def move_player(move_y, move_x):
 	if new_y >= 0 and new_y <= map_y - 1 and new_x >= 0 and new_x <= map_x - 1:
 		if not game_map[new_y][new_x][0][0] in collision_objects:
 			if len(game_map[y][x]) == 1:
-				game_map[y][x][0] = ["filler", filler]
+				game_map[y][x][0] = ["filler", game_icons["filler"]]
 			else:
 				del(game_map[y][x][0])
-			make_object("player", player_icon, new_y, new_x)
+			make_object("player", game_icons["player"], new_y, new_x)
+
 			#adjusts camera
-			adjust = False
-			if new_y - camera_buffer_y < camera_y and move_y < 0:
-				adjust = True
-			elif new_y + camera_buffer_y > camera_y + screen_y - 1 and move_y > 0:
-				adjust = True
-			if new_x - camera_buffer_x < camera_x and move_x < 0:
-				adjust = True
-			elif new_x + camera_buffer_x > camera_x + screen_x - 1 and move_x > 0:
-				adjust = True
-			if adjust:
-				move_camera(move_y, move_x)			
+			if on_screen("player"):
+				adjust = False
+				if new_y - camera_buffer_y < camera_y and move_y < 0:
+					adjust = True
+				elif new_y + camera_buffer_y > camera_y + screen_y - 1 and move_y > 0:
+					adjust = True
+				if new_x - camera_buffer_x < camera_x and move_x < 0:
+					adjust = True
+				elif new_x + camera_buffer_x > camera_x + screen_x - 1 and move_x > 0:
+					adjust = True
+				if adjust:
+					move_camera(move_y, move_x)
+			else:
+				center_camera()
 
 #checks boundaries and moves the camera accordingly
 def move_camera(move_y, move_x):
 	global camera_y
 	global camera_x
-	new_y = camera_y + move_y
-	new_x = camera_x + move_x
+	new_y = int(camera_y + move_y)
+	new_x = int(camera_x + move_x)
 	if move_y < 0:
 		camera_y = max(0, new_y)
 	else:
@@ -162,14 +166,21 @@ def move_camera(move_y, move_x):
 	else:
 		camera_x = min(camera_max_x, new_x)
 
+#centers camera on player
+def center_camera():
+	global camera_y
+	global camera_x
+	camera_y, camera_x, = find_object("player")[0]
+	move_camera(-12, -40)
+
 #builds a border wall
 def make_border_wall():
 	for a in range(map_y):
-		make_object("border", wall_icon, a, 0)
-		make_object("border", wall_icon, a, map_x - 1)
+		make_object("border", game_icons["wall"], a, 0)
+		make_object("border", game_icons["wall"], a, map_x - 1)
 	for a in range(map_x):
-		make_object("border", wall_icon, 0, a)
-		make_object("border", wall_icon, map_y - 1, a)
+		make_object("border", game_icons["wall"], 0, a)
+		make_object("border", game_icons["wall"], map_y - 1, a)
 
 #startup functions
 def game_startup():
@@ -177,9 +188,9 @@ def game_startup():
 	make_map()
 	make_screen()
 	make_border_wall()
-	make_object("player", player_icon, 4, 4)
-	make_rand_objects(100, "wall", wall_icon, 0, 0, map_y, map_x)
-	make_rand_objects(50, "door", door_icon, 0, 0, map_y, map_x, ["filler", "wall"])
+	make_object("player", game_icons["player"], 4, 4)
+	make_rand_objects(100, "wall", game_icons["wall"], 0, 0, map_y, map_x)
+	make_rand_objects(50, "door", game_icons["door"], 0, 0, map_y, map_x, ["filler", "wall"])
 	game_state = "game_loop"
 
 #handles input and basic movement logic
@@ -226,6 +237,15 @@ def input_logic():
 	elif key == right:
 		move_camera(0, camera_speed)
 
+#returns true if object is in game_screen
+def on_screen(object):
+	icon = game_icons[object]
+	for a in range(screen_y):
+		for b in range(screen_x):
+			if game_screen[a][b] == icon:
+				return True
+	return False
+
 #this is the game loop
 def game_loop():
 	global game_state
@@ -234,6 +254,7 @@ def game_loop():
 		update_screen()
 		draw_screen()
 		input_logic()
+		
 
 #this is the main loop
 game_state = "game_startup"
